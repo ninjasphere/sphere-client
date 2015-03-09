3f84e55314e85ecde8972e15929662819ac124e8
Jon Seymour <jon.seymour@gmail.com>
2015-03-09 14:17:43 +1100

    Add async support.

--- command/command.go
+++ command/command.go
@@ -7,6 +7,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/ninjasphere/go-ninja/api"
+	"github.com/ninjasphere/go-ninja/logger"
 	"os"
 )
 
@@ -20,6 +21,7 @@ type Configuration struct {
 type Request struct {
 	Command string   `json:"command,omitempty"`
 	Args    []string `json:"args,omitempty"`
+	Async   bool     `json:"async,omitempty"`
 }
 
 // Format of a response from a command.
@@ -28,6 +30,7 @@ type Response struct {
 	Output *[]string   `json:"output,omitempty"` // present if the command does not generate json structured output
 	Error  *[]string   `json:"error,omitempty"`  // any output written to the commands stderr
 	Status *int        `json:"status,omitempty"` // the exit status of the command.
+	Pid    *int        `json:"pid,omitempty"`    // the PID of the started or completed command.
 }
 
 // The command service.
@@ -35,6 +38,8 @@ type CommandChannel struct {
 	config Configuration
 	sender func(event string, payload ...interface{}) error
 }
+
+var log = logger.GetLogger("sphere-client.command")
 
 // Exports the command service into the bus
 func ExportCommandChannel(config *Configuration) error {
@@ -75,18 +80,41 @@ func (cs *CommandChannel) Exec(request *Request) (*Response, error) {
 		args[1] = "exec"
 		args[2] = request.Command
 		copy(args[3:], request.Args)
-		if _, err := os.StartProcess(cs.config.HelperPath, args, procAttr); err != nil {
+		if proc, err := os.StartProcess(cs.config.HelperPath, args, procAttr); err != nil {
 			return nil, err
+		} else {
+
+			fromParent.Close()
+			toParent.Close()
+
+			toChild.Close()
+
+			response := &Response{
+				Pid: &proc.Pid,
+			}
+
+			executor := func(response *Response) error {
+
+				err := json.NewDecoder(fromChild).Decode(response)
+				if err != nil {
+					log.Errorf("failed to parse response from command: %v", err)
+				}
+
+				cs.sender("completed", response)
+				fromChild.Close()
+				return err
+			}
+
+			if request.Async {
+				asyncResponse := &Response{
+					Pid: &proc.Pid,
+				}
+				go executor(asyncResponse)
+				return response, nil
+			}
+
+			return response, executor(response)
 		}
-
-		fromParent.Close()
-		toParent.Close()
-
-		toChild.Close()
-
-		response := &Response{}
-		err := json.NewDecoder(fromChild).Decode(response)
-		return response, err
 	}
 
 }
