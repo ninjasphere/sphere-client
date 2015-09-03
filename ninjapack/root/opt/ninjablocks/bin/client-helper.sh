486b9dfee5d0c24f79e93aedeeef8ea596ae028c
Jon Seymour <jon@ninjablocks.com>
2015-09-03 19:45:51 +1000

    Move derivation of client arguments into client-helper.sh
    The intent is to allow a .sh file in /etc/profile.d to determine the value of
    NINJA_SPHERE_CLIENT_ARGS and default it to cloud production otherwise.
    
    Signed-off-by: Jon Seymour <jon@ninjablocks.com>

--- ninjapack/root/opt/ninjablocks/bin/client-helper.sh
+++ ninjapack/root/opt/ninjablocks/bin/client-helper.sh
@@ -10,11 +10,33 @@ die() {
  	exit 1
 }
 
+client-args() {
+	(
+	    test -r /etc/profile && . /etc/profile
+	    test -r /etc/ninja-hardware && . /etc/ninja-hardware
+	    test -r /etc/ninja-release && . /etc/ninja-release
+	    test -r /etc/default/ninja && . /etc/default/ninja
+
+	    if test -z "$NINJA_SPHERE_CLIENT_ARGS"; then
+		    TARGET_BRANCH=`echo $NINJA_OS_BUILD_TARGET | cut -d'-' -f2`
+		    case "$TARGET_BRANCH" in
+		    	stable|testing)
+			    	NINJA_SPHERE_CLIENT_ARGS="--cloud-production"
+				;;
+		    esac
+		fi
+		test -n "$NINJA_SPHERE_CLIENT_ARGS" && echo $NINJA_SPHERE_CLIENT_ARGS
+	)
+}
+
 # parse the command arguments and invoke handling functions as appropriate
 main() {
 	local cmd=$1
 	shift 1
 	case "$cmd" in
+	client-args)
+		client-args
+	;;
 	apply-site-preferences)
 		rc=0
 		test -d "$SPHERE_CLIENT_SITE_PREFERENCE_HELPERS" && cd "$SPHERE_CLIENT_SITE_PREFERENCE_HELPERS" && find . -type f -maxdepth 1 -exec basename {} \; | sort | while read c; do
